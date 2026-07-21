07d31c1af166936ba61ceb7fb05cb3040ed5b197
oridim <125223080+oridim@users.noreply.github.com>
2026-07-21 18:52:06 -0400

    refactor: use new formatting utility

--- cli/commands/playlists/now-playing.ts
+++ cli/commands/playlists/now-playing.ts
@@ -1,8 +1,11 @@
 import { command, number, positional } from '@drizzle-team/brocli';
 import { M3uParser } from 'm3u-parser-generator';
 
-import { getZonedDateTimeFromEpochMilliseconds } from '@/lib/utilities/datetime.ts';
-import { determineNowPlaying } from '@/lib/utilities/playlists.ts';
+import {
+    formatPlaytimeDuration,
+    getZonedDateTimeFromEpochMilliseconds,
+} from '@/lib/utilities/datetime.ts';
+import { determineNowPlaying as determineNowPlayingTrack } from '@/lib/utilities/playlists.ts';
 import { EXIT_CODES } from '@/cli/utilities/process.ts';
 
 import LOGGER from '@/cli/utilities/logger.ts';
@@ -54,9 +57,13 @@ export default command({
 
         const parser = new M3uParser();
         const playlist = parser.parse(playlistText);
-        const nowPlaying = determineNowPlaying(playlist, zonedDateTime);
 
-        if (!nowPlaying) {
+        const nowPlayingTrack = determineNowPlayingTrack(
+            playlist,
+            zonedDateTime,
+        );
+
+        if (!nowPlayingTrack) {
             LOGGER.info(
                 `Playlist file '${playlistFile}' is empty or ran out of tracks.`,
             );
@@ -64,46 +71,24 @@ export default command({
             Deno.exit(EXIT_CODES.invalidValue);
         }
 
-        const seekDuration = Temporal.Duration
-            .from({
-                milliseconds: nowPlaying.seekTime,
-            })
-            .round({
-                largestUnit: 'hour',
-                smallestUnit: 'second',
-                roundingMode: 'trunc',
-            });
-
-        const trackDuration = Temporal.Duration
-            .from({
-                milliseconds: nowPlaying.trackDuration,
-            })
-            .round({
-                largestUnit: 'hour',
-                smallestUnit: 'second',
-                roundingMode: 'trunc',
-            });
-
-        const durationFormatter = new Intl.DurationFormat('en-US', {
-            style: 'digital',
-        });
-
         console.log('Now Playing Status:');
-        console.log(`  File:      ${nowPlaying.filePath}`);
+        console.log(`  File:      ${nowPlayingTrack.filePath}`);
         console.log(
-            `  Track:     ${nowPlaying.index + 1} of ${playlist.medias.length}`,
+            `  Track:     ${
+                nowPlayingTrack.index + 1
+            } of ${playlist.medias.length}`,
         );
 
-        if (nowPlaying.group) {
-            console.log(`  Bucket:    ${nowPlaying.group}`);
+        if (nowPlayingTrack.group) {
+            console.log(`  Bucket:    ${nowPlayingTrack.group}`);
         }
 
         console.log(
-            `  Position:  ${durationFormatter.format(seekDuration)} / ${
-                durationFormatter.format(trackDuration)
-            } (${Math.floor(nowPlaying.seekTime / 1000)}s / ${
-                Math.floor(nowPlaying.trackDuration / 1000)
-            }s)`,
+            `  Position:  ${
+                formatPlaytimeDuration(nowPlayingTrack.seekTime)
+            } / ${formatPlaytimeDuration(nowPlayingTrack.trackDuration)} (${
+                Math.floor(nowPlayingTrack.seekTime / 1000)
+            }s / ${Math.floor(nowPlayingTrack.trackDuration / 1000)}s)`,
         );
     },
 });
