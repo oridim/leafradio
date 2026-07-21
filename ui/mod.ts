d7c84f6a803f9bf4f7a4ebe14da9194079f57d11
oridim <125223080+oridim@users.noreply.github.com>
2026-07-21 11:17:05 -0400

    remove: initial UI support
    I am not going to have time to work on this project too much. So, UI is being axed in favor of getting a straight-forward CLI working.

--- ui/mod.ts
+++
@@ -1,6 +0,0 @@
-import { load as loadRFD } from '@miyauci/rfd/deno';
-
-export default async () => {
-    console.log('[LeafRadio] Initializing Rusty File Dialog...');
-    await loadRFD();
-};
--- ui/signals.ts
+++
@@ -1,7 +0,0 @@
-import type { DeepPartial } from '@/lib/utilities/types.ts';
-
-export type PartialSignals = DeepPartial<Signals>;
-
-export interface Signals {}
-
-export default {} satisfies Signals;
