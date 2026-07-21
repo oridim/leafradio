584ba53f726b20b101d9e70803b0e1c12f9b9152
oridim <125223080+oridim@users.noreply.github.com>
2026-07-21 13:33:30 -0400

    chore: bikeshedding coding style

--- cli/commands/playlists/generate.ts
+++ cli/commands/playlists/generate.ts
@@ -23,104 +23,123 @@ import {
 } from '@/shared/database/mod.ts';
 
 const COMMAND_OPTIONS = {
-    directoryPath: positional('directory-path').desc(
-        'directory path to build a playlist from',
-    )
+    directoryPath: positional('directory-path')
+        .desc('directory path to build a playlist from')
         .required(),
 
-    outputFile: string('output-file').desc(
-        'sets the file to output the playlist to',
-    ),
+    outputFile: string('output-file')
+        .desc('sets the file to output the playlist to'),
 
-    energyCurve: string('energy-curve').desc(
-        'sets the distribution curve forumla to determine track inclusion in a bucket',
-    ).enum(
-        // **HACK:** `enum` definition function expects at least one non-dynamic
-        // string element as the first element. Brocli is trying to enforce that
-        // there is at least one string element.
-        ...Object.values(ENERGY_CURVE_NAMES) as [string, ...string[]],
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .energyCurve,
-    ),
+    energyCurve: string('energy-curve')
+        .desc(
+            'sets the distribution curve forumla to determine track inclusion in a bucket',
+        )
+        .enum(
+            // **HACK:** `enum` definition function expects at least one non-dynamic
+            // string element as the first element. Brocli is trying to enforce that
+            // there is at least one string element.
+            ...Object.values(ENERGY_CURVE_NAMES) as [string, ...string[]],
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .energyCurve,
+        ),
 
-    maxTracksPerBucket: number('max-tracks-per-bucket').desc(
-        'sets the maximum amount of tracks per bucket',
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .maxTracksPerBucket,
-    ),
+    maxTracksPerBucket: number('max-tracks-per-bucket')
+        .desc('sets the maximum amount of tracks per bucket')
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .maxTracksPerBucket,
+        ),
 
-    minimumDuration: number('minimum-duration').desc(
-        'sets the minimum duration (in milliseconds) a track requires to be included',
-    ).default(0),
+    minimumDuration: number('minimum-duration')
+        .desc(
+            'sets the minimum duration (in milliseconds) a track requires to be included',
+        )
+        .default(0),
 
-    mixingRule: string('mixing-rule').desc(
-        'sets the sorting algorithm used to determine track distribution inside of buckets',
-    ).enum(
-        // **HACK:** See above note on `energyCurve`.
-        ...Object.values(MIXING_RULE_NAMES) as [string, ...string[]],
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .mixingRule,
-    ),
+    mixingRule: string('mixing-rule')
+        .desc(
+            'sets the sorting algorithm used to determine track distribution inside of buckets',
+        )
+        .enum(
+            // **HACK:** See above note on `energyCurve`.
+            ...Object.values(MIXING_RULE_NAMES) as [string, ...string[]],
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .mixingRule,
+        ),
 
-    numberOfBuckets: number('number-of-buckets').desc(
-        'sets how many buckets the linked repositories of tracks are split into',
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .numberOfBuckets,
-    ),
+    numberOfBuckets: number('number-of-buckets')
+        .desc(
+            'sets how many buckets the linked repositories of tracks are split into',
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .numberOfBuckets,
+        ),
 
-    pacingStrictnessArousal: number('pacing-strictness-arousal').desc(
-        'sets how closely the track distribution must follow the energy curve',
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .pacingStrictnessArousal,
-    ),
+    pacingStrictnessArousal: number('pacing-strictness-arousal')
+        .desc(
+            'sets how closely the track distribution must follow the energy curve',
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .pacingStrictnessArousal,
+        ),
 
-    pacingStrictnessValence: number('pacing-strictness-valence').desc(
-        'sets how closely the track distribution must follow the vibe target',
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .pacingStrictnessValence,
-    ),
+    pacingStrictnessValence: number('pacing-strictness-valence')
+        .desc(
+            'sets how closely the track distribution must follow the vibe target',
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .pacingStrictnessValence,
+        ),
 
-    scoreFuzziness: number('score-fuzziness').desc(
-        "sets how exacting a track's scoring must match for inclusion by introducing randomness",
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .scoreFuzziness,
-    ),
+    scoreFuzziness: number('score-fuzziness')
+        .desc(
+            "sets how exacting a track's scoring must match for inclusion by introducing randomness",
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .scoreFuzziness,
+        ),
 
-    seed: number('seed').desc(
-        'sets the seed used for the random number generator',
-    ).default(
-        Temporal.Now.zonedDateTimeISO().with({
-            hour: 0,
-            minute: 0,
-            second: 0,
-            millisecond: 0,
-        }).epochMilliseconds,
-    ),
+    seed: number('seed')
+        .desc('sets the seed used for the random number generator')
+        .default(
+            Temporal.Now.zonedDateTimeISO().with({
+                hour: 0,
+                minute: 0,
+                second: 0,
+                millisecond: 0,
+            }).epochMilliseconds,
+        ),
 
-    targetDurationPerBucket: number('target-duration-per-bucket').desc(
-        'sets the max possible cumulative duration of individual buckets',
-    ),
+    targetDurationPerBucket: number('target-duration-per-bucket')
+        .desc(
+            'sets the max possible cumulative duration of individual buckets',
+        ),
 
-    trackSpacing: number('track-spacing').desc(
-        'sets how much time (in milliseconds) is padded between each track in a bucket',
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .trackSpacing,
-    ),
+    trackSpacing: number('track-spacing')
+        .desc(
+            'sets how much time (in milliseconds) is padded between each track in a bucket',
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .trackSpacing,
+        ),
 
-    vibeTarget: number('vibe-target').desc(
-        'sets how weighted tracks included from linked repositories towards positive or negative vibes are',
-    ).default(
-        DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
-            .vibeTarget,
-    ),
+    vibeTarget: number('vibe-target')
+        .desc(
+            'sets how weighted tracks included from linked repositories towards positive or negative vibes are',
+        )
+        .default(
+            DEFAULT_SERIALIZED_PACK_PLAYLIST_BUCKETS_PARAMETERS
+                .vibeTarget,
+        ),
 } as const;
 
 function determineTargetBucketDuration(numberOfBuckets: number): number {
