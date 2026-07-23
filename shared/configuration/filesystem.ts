3a9f2a47313d06806fd9e93b91ee5769de49d101
oridim <125223080+oridim@users.noreply.github.com>
2026-07-23 00:22:45 -0400

    remove: XDG compliance library due to not using it anymore

--- shared/configuration/filesystem.ts
+++ shared/configuration/filesystem.ts
@@ -1,15 +1,1 @@
-import { join } from '@std/path';
-
-import { Adapt } from '@404wolf/xdg-portable';
-
-import { APPLICATION_IDENTIFIER } from '@/shared/configuration/application.ts';
-
-const { cache } = Adapt();
-
-const XDG_CACHE_HOME = cache();
-
 export const FILE_NAME_AUDIO_DATA = '.leafradio.audio-data.json';
-
-export const DIRECTORY_DATA = join(XDG_CACHE_HOME, APPLICATION_IDENTIFIER);
-
-export const FILE_AUDIO_DATA = join(DIRECTORY_DATA, 'audio-data.json');
