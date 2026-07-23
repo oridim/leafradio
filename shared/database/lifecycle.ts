feb65616cd14fa343b9c8adcfc955374631f4743
oridim <125223080+oridim@users.noreply.github.com>
2026-07-22 21:48:34 -0400

    remove: SQLite3 + MikoORM dep

--- shared/database/lifecycle.ts
+++
@@ -1,8 +0,0 @@
-import { DATABASE, ENTITY_MANAGER } from '@/shared/database/orm.ts';
-
-export async function initDatabase(): Promise<void> {
-    await ENTITY_MANAGER.execute('PRAGMA journal_mode = WAL;');
-    await DATABASE.migrator.up();
-
-    await Promise.all([]);
-}
